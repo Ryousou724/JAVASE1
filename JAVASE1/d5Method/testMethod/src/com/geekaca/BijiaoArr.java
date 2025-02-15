d0a5d869b89fb31dfd3cb3028a72c11a735abf57
Ryousou724 <ryousou724@gmail.com>
2025-02-15 16:53:43 +0900

    方法作业

--- JAVASE1/d5Method/testMethod/src/com/geekaca/BijiaoArr.java
+++ JAVASE1/d5Method/testMethod/src/com/geekaca/BijiaoArr.java
@@ -1,2 +1,27 @@
-package com.geekaca;public class BijiaoArr {
+package com.geekaca;
+
+public class BijiaoArr {
+    public static void main(String[] args) {
+        /**
+         * homework 利用方法比较数组是否相同
+         */
+        int[] arr1 = {10, 20, 30};
+        int[] arr2 = {10, 20, 30};
+        System.out.println(compare(arr1, arr2));
+    }
+    public static boolean compare(int[] arr1, int[] arr2) {
+        if (arr1.length != arr2.length) {
+            return false;
+        }
+        if (arr1 == null || arr2 == null) {
+            return false;
+        }
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]){
+                return false;
+            }
+        }
+        return true;
+    }
 }
+
--- JAVASE1/d5Method/testMethod/src/com/geekaca/ChaxunArr.java
+++ JAVASE1/d5Method/testMethod/src/com/geekaca/ChaxunArr.java
@@ -1,2 +1,20 @@
-package com.geekaca;public class ChaxunArr {
+package com.geekaca;
+
+public class ChaxunArr {
+    public static void main(String[] args) {
+        /**
+         * homework 从数组中查询制定元素的索引
+         */
+        int[] arr = {11, 22, 33, 44, 55};
+        int num = length(arr, 33);
+        System.out.println("元素的索引是" + num);
+    }
+    public static int length(int[] arr, int length){
+        for (int i = 0; i <= arr.length; i++) {
+            if (arr[i] == length){
+                return i;
+            }
+        }
+        return -1;
+    }
 }
